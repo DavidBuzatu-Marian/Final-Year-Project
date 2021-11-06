87deecff3fff3d99db9c81c7fbf6f5498c7a19be
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-11-06 12:18:28 +0000

    Addded NNModel class

---
+++ flask_server/nn_model/nn_model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class NNModel(nn.Module):
+    def __init__(self, layer_types, parameters, activations_types):
+        super (NNModel, self).__init__() # needed for how torch works
+
+        for layer, layer_params in zip(layer_types, parameters):
+            self.layers.append(NNLayerFactory(layer, layer_params))
+        for activation_type in activations_types:
+            self.activations.append(NNActivationFactory(activation_type))
