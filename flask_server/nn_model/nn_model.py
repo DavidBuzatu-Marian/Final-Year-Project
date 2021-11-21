781586fce6de0a48ada2488b3deb13414ebf9ecb
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-11-21 20:50:46 +0000

    Added initial nn model implementation

--- flask_server/nn_model/nn_model.py
+++ flask_server/nn_model/nn_model.py
@@ -1,17 +1,39 @@
+from flask_server.nn_model.nn_activation_factory import NNActivationFactory
+from flask_server.nn_model.nn_layer_factory import NNLayerFactory
 import torch
 import torch.nn as nn
 
 
 class NNModel(nn.Module):
-    def __init__(self, layer_types, parameters, activations_types):
+    def __init__(self, architecture_components):
         super(NNModel, self).__init__()  # needed for how torch works
-
-        for layer, layer_params in zip(layer_types, parameters):
-            self.layers.append(NNLayerFactory(layer, layer_params))
-        for activation_type in activations_types:
-            self.activations.append(NNActivationFactory(activation_type))
+        layer_factory = NNLayerFactory()
+        activation_factory = NNActivationFactory()
+        for component_type, component_details in architecture_components.items():
+            if component_type == "layer":
+                self.model.append(
+                    layer_factory.get_layer(
+                        component_details["layer_type"],
+                        component_details["subtype"],
+                        component_details["parameters"],
+                    )
+                )
+            elif component_type == "activation":
+                self.model.append(
+                    activation_factory.get_activation(
+                        component_details["activation_type"],
+                        component_details["parameters"],
+                    )
+                )
+            elif component_type == "concatenate":
+                pass
+            elif component_type == "optimizer":
+                pass
+            else:
+                raise Exception("Invalid component type")
 
     def forward(self, input_data):
-        for layer, activation in zip(self.layers, self.activations):
-            input_data = activation(layer(input_data))
-        return input_data
+        prediction = input_data
+        for component in self.model:
+            prediction = component(prediction)
+        return prediction
