e9dde56d936e8acce2ff2cfeafa95081eaf3c7bf
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-11-25 20:53:52 +0000

    Tried saving model to redis. Failing because of serialization

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from flask import request
-
+from flask.helpers import url_for
+from flask.json import jsonify
+import redis
 import sys
+import os
+from dotenv import load_dotenv
 
 sys.path.insert(0, "./nn_model")
 
 from nn_model import NNModel
 
+load_dotenv()
+
+redis_instance = redis.Redis(
+    host=os.environ.get("CACHE_REDIS_HOST"),
+    port=os.environ.get("CACHE_REDIS_PORT"),
+    db=os.environ.get("CACHE_REDIS_DB"),
+)
 app = Flask(__name__)
 
 
 @app.route("/model/create", methods=["POST"])
 def model_create():
     model = NNModel(request.json)
-    return repr(model._model)
+    redis_instance.set("model", model)
+    # TODO: Check if there is a saved model and delete it
+    return jsonify(model)
 
 
 @app.route("/")
