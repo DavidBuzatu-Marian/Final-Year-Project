35f23512b8ef78169315c258dc3719efe9ebaa42
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-11-25 18:25:43 +0000

    Added tests for model. Changed model input type

--- flask_server/nn_model/nn_model.py
+++ flask_server/nn_model/nn_model.py
@@ -11,28 +11,29 @@ class NNModel(nn.Module):
         activation_factory = NNActivationFactory()
         self._model = []
 
-        for component_type, component_details in architecture_components.items():
-            if component_type == "layer":
-                self._model.append(
-                    layer_factory.get_layer(
-                        component_details["layer_type"],
-                        component_details["subtype"],
-                        component_details["parameters"],
+        for layer_dict in architecture_components["network"]:
+            for component_type, component_details in layer_dict.items():
+                if component_type == "layer":
+                    self._model.append(
+                        layer_factory.get_layer(
+                            component_details["layer_type"],
+                            component_details["subtype"],
+                            component_details["parameters"],
+                        )
                     )
-                )
-            elif component_type == "activation":
-                self._model.append(
-                    activation_factory.get_activation(
-                        component_details["activation_type"],
-                        component_details["parameters"],
+                elif component_type == "activation":
+                    self._model.append(
+                        activation_factory.get_activation(
+                            component_details["activation_type"],
+                            component_details["parameters"],
+                        )
                     )
-                )
-            elif component_type == "concatenate":
-                pass
-            elif component_type == "optimizer":
-                pass
-            else:
-                raise Exception("Invalid component type")
+                elif component_type == "concatenate":
+                    pass
+                elif component_type == "optimizer":
+                    pass
+                else:
+                    raise Exception("Invalid component type")
 
     def forward(self, input_data):
         prediction = input_data
--- flask_server/nn_model/tests/test_model.py
+++ flask_server/nn_model/tests/test_model.py
@@ -13,25 +13,115 @@ class TestModel(unittest.TestCase):
     def test_single_layer_convolution(self):
         model = NNModel(
             {
-                "layer": {
-                    "layer_type": "Convolution",
-                    "subtype": "Conv2d",
-                    "parameters": {
-                        "in_channels": 16,
-                        "out_channels": 33,
-                        "kernel_size": 3,
-                        "stride": 2,
-                    },
-                }
+                "network": [
+                    {
+                        "layer": {
+                            "layer_type": "Convolution",
+                            "subtype": "Conv2d",
+                            "parameters": {
+                                "in_channels": 16,
+                                "out_channels": 33,
+                                "kernel_size": 3,
+                                "stride": 2,
+                            },
+                        }
+                    }
+                ]
             }
         )
 
         pytorch_model = nn.Sequential(nn.Conv2d(16, 33, 3, stride=2))
-        model.eval()
-
+        self.assertEqual(len(model._model), len(pytorch_model))
         for idx in range(len(model._model)):
             self.assertEqual(repr(model._model[idx]), repr(pytorch_model[idx]))
 
-    def init_weights(self, model):
-        for m in model.modules():
-            torch.nn.init.xavier_uniform(m.weight)
+    def test_single_convolution_with_activation_convolution(self):
+        model = NNModel(
+            {
+                "network": [
+                    {
+                        "layer": {
+                            "layer_type": "Convolution",
+                            "subtype": "Conv2d",
+                            "parameters": {
+                                "in_channels": 16,
+                                "out_channels": 33,
+                                "kernel_size": 3,
+                                "stride": 2,
+                            },
+                        }
+                    },
+                    {
+                        "activation": {
+                            "activation_type": "ReLU",
+                            "parameters": {
+                                "random": 16,
+                            },
+                        }
+                    },
+                ]
+            }
+        )
+
+        pytorch_model = nn.Sequential(nn.Conv2d(16, 33, 3, stride=2), nn.ReLU())
+        self.assertEqual(len(model._model), len(pytorch_model))
+        for idx in range(len(model._model)):
+            self.assertEqual(repr(model._model[idx]), repr(pytorch_model[idx]))
+
+    def test_double_convolution_with_activation_convolution(self):
+        model = NNModel(
+            {
+                "network": [
+                    {
+                        "layer": {
+                            "layer_type": "Convolution",
+                            "subtype": "Conv2d",
+                            "parameters": {
+                                "in_channels": 16,
+                                "out_channels": 33,
+                                "kernel_size": 3,
+                                "stride": 2,
+                            },
+                        }
+                    },
+                    {
+                        "activation": {
+                            "activation_type": "ReLU",
+                            "parameters": {
+                                "random": 16,
+                            },
+                        }
+                    },
+                    {
+                        "layer": {
+                            "layer_type": "Convolution",
+                            "subtype": "Conv2d",
+                            "parameters": {
+                                "in_channels": 16,
+                                "out_channels": 33,
+                                "kernel_size": 3,
+                                "stride": 2,
+                            },
+                        }
+                    },
+                    {
+                        "activation": {
+                            "activation_type": "ReLU",
+                            "parameters": {
+                                "random": 16,
+                            },
+                        }
+                    },
+                ]
+            }
+        )
+
+        pytorch_model = nn.Sequential(
+            nn.Conv2d(16, 33, 3, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(16, 33, 3, stride=2),
+            nn.ReLU(),
+        )
+        self.assertEqual(len(model._model), len(pytorch_model))
+        for idx in range(len(model._model)):
+            self.assertEqual(repr(model._model[idx]), repr(pytorch_model[idx]))
