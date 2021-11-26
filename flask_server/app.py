46bf6977dfe85e8b3880ad058148f30662f0f94f
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-11-26 19:00:02 +0000

    Removed redis cache. Changed to saving model to file

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask import request
-from flask.helpers import url_for
+from logging import error
+from flask import Flask, request
 from flask.json import jsonify
-import redis
+import torch
 import sys
 import os
 from dotenv import load_dotenv
@@ -13,22 +12,25 @@ from nn_model import NNModel
 
 load_dotenv()
 
-redis_instance = redis.Redis(
-    host=os.environ.get("CACHE_REDIS_HOST"),
-    port=os.environ.get("CACHE_REDIS_PORT"),
-    db=os.environ.get("CACHE_REDIS_DB"),
-)
 app = Flask(__name__)
 
 
 @app.route("/model/create", methods=["POST"])
 def model_create():
     model = NNModel(request.json)
-    redis_instance.set("model", model)
     # TODO: Check if there is a saved model and delete it
-    return jsonify(model)
+    path = os.getenv("MODEL_PATH")
+    print(os.path.join("", path))
+    if os.path.isfile(path):
+        os.remove(path)
+    torch.save(model, path)
+    return jsonify(repr(model._model))
 
 
 @app.route("/")
 def hello_world():
-    return "Hello!"
+    path = os.getenv("MODEL_PATH")
+    if os.path.isfile(path):
+        model = torch.load(path)
+        return jsonify(repr(model._model))
+    raise error("Error. No model found")
