7ad9e9c598bf618a33706f647568d9def30bd2a4
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-12-02 16:48:06 +0000

    Added data loader and helpers for training. Added training logic

--- flask_server/helpers/app_helpers.py
+++ flask_server/helpers/app_helpers.py
@@ -1,4 +1,7 @@
 import os
+from nn_loss.nn_loss_factory import NNLossFactory
+from nn_optimizer.nn_optimizer_factory import NNOptimizerFactory
+
 import torch
 from flask.json import jsonify
 from logging import error
@@ -18,3 +21,24 @@ def save_file(file, path_env):
     path = os.getenv(path_env)
     Path(path).mkdir(parents=True, exist_ok=True)
     file.save(os.path.join(path, filename))
+
+
+def get_loss(request_json):
+    loss_factory = NNLossFactory()
+    for loss_type, loss_parameters in request_json["loss"]:
+        return loss_factory.get_loss(loss_type=loss_type, parameters=loss_parameters)
+    raise Exception("No loss function set")
+
+
+def get_optimizer(request_json):
+    optimizer_factory = NNOptimizerFactory()
+    for optimizer_type, optimizer_parameters in request_json["optimizer"]:
+        return optimizer_factory.get_optimizer(
+            optimizer_type=optimizer_type, parameters=optimizer_parameters
+        )
+    raise Exception("No optimizer set")
+
+
+def get_hyperparameters(request_json):
+    # TODO: Think how to represent hyperparams
+    return {"epochs": 2, "num_workers": 1, "batch_size": 2, "shuffle": True}
--- flask_server/routes/model.py
+++ flask_server/routes/model.py
@@ -3,8 +3,10 @@ from flask import Flask, request
 from flask.json import jsonify
 import os
 import sys
-from helpers.app_helpers import read_model_from_path, save_file
+from helpers.data_helpers import *
+from helpers.app_helpers import *
 import torch
+
 
 try:
     from nn_model import NNModel
@@ -28,8 +30,25 @@ def model_create():
 def model_train():
     path = os.getenv("MODEL_PATH")
     model = read_model_from_path(path)
-    # Get data from database/local store
-    # Get optimizer from request
-    # Get loss from request
-    # Get training options - epochs - currently set to default of 5
-    # Save model
+    loss_func = get_loss(request.json)
+    optimizer = get_optimizer(request.json)
+    hyperparameters = get_hyperparameters(request.json)
+
+    train_dataloader = get_dataloader(
+        data_path=os.getenv("TRAIN_DATA_PATH"),
+        labels_path=os.getenv("TRAIN_LABELS_PATH"),
+        hyperparameters=hyperparameters,
+    )
+    for _ in range(0, hyperparameters["epochs"]):
+        for data, label in train_dataloader:
+            optimizer.zero_grad()
+
+            data = reshape_data(data, hyperparameters)
+            data = normalize_data(data, hyperparameters)
+
+            output = model(data)
+            loss = loss_func(output, label)
+            loss.backward()
+            optimizer.step()
+
+    torch.save(model, path)
