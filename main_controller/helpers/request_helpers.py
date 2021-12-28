b14524cece0ce171b9d13cc36b92b949b7e61662
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-12-28 18:40:07 +0200

    Updated request helpers instance data creation

--- main_controller/helpers/request_helpers.py
+++ main_controller/helpers/request_helpers.py
@@ -9,7 +9,7 @@ async def post_to_instance(url, session, data):
     return response.text()
 
 
-async def post_data_distribution(files, environment_data_distribution):
+def get_instance_data_from_files(files, data_distribution):
     data_keys = [
         "train_data",
         "train_labels",
@@ -18,15 +18,20 @@ async def post_data_distribution(files, environment_data_distribution):
         "test_data",
         "test_labels",
     ]
+    instance_data = dict()
+    return {
+        key: instance_data.get(key, []).append(data[i])
+        for key in data_keys
+        for data in files.getlist(key)
+        for i in data_distribution
+    }
+
+
+async def post_data_distribution(files, environment_data_distribution):
     async with ClientSession() as client_session:
         post_requests = []
         for environment_ip, data_distribution in environment_data_distribution.items():
-            instance_data = [
-                {key: data[i]}
-                for key in data_keys
-                for data in files.getlist(key)
-                for i in data_distribution
-            ]
+            instance_data = get_instance_data_from_files(files, data_distribution)
             post_requests.append(
                 post_to_instance(
                     environment_ip + "/dataset/distribution",
