8aac816016e16fefb71fb7dd25a0c0e52c560799
davidbuzatu-marian <davidm.buzatu@gmail.com>
2022-01-28 19:32:33 +0000

    Added worker and endpoint for environment creation task

--- backend_server/routes/environment.js
+++ backend_server/routes/environment.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { environmentCreateQueue } = require('../workers/environment_create');
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
-  res.json({ user_id: req.headers['x-auth'] });
+  const job = await environmentCreateQueue.add({ headers: req.headers });
+  return res.status(202).json({ jobLink: `/api/environment/create${job.id}` });
+});
+
+router.get('/create/:id', async (req, res) => {
+  const id = req.params.id;
+  const job = await environmentCreateQueue.getJob(id);
+  if (job === null) {
+    return res.status(400).end();
+  } else {
+    const jobState = await job.getState();
+    const jobFailReason = job.failedReason;
+    return res.json({ id, jobState, jobFailReason });
+  }
 });
 
 module.exports = router;
---
+++ backend_server/workers/environment_create.js
@@ -0,0 +1,22 @@
+const Queue = require('bull');
+const config = require('config');
+
+const environmentCreateQueue = new Queue('environment-create-queue', {
+  redis: {
+    port: config.get('redisPort'),
+    host: config.get('redisIP'),
+    password: config.get('redisPassword'),
+  },
+});
+
+environmentCreateQueue.process(async (job) => {
+  const res = await axios.post(
+    `http://${config.get('loadBalancerIP')}:${config.get(
+      'loadBalancerPort'
+    )}/environment/create`,
+    { headers: job.data.headers }
+  );
+  done(null, res.data);
+});
+
+module.exports = { environmentCreateQueue };
