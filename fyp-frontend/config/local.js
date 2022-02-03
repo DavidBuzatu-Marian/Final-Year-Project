42c9833007c5770b0fdb08bf819b8ff4e7d6b161
davidbuzatu-marian <davidm.buzatu@gmail.com>
2022-02-03 21:44:01 +0000

    Added authenticated url

--- fyp-frontend/config/local.js
+++ fyp-frontend/config/local.js
@@ -1,4 +1,5 @@
 export const urls = {
   registerUrl: 'http://localhost:5002/api/auth/register',
   loginUrl: 'http://localhost:5002/api/auth/login',
+  authenticatedUrl: 'http://localhost:5002/api/auth/authenticated',
 };
