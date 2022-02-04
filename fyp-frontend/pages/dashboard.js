198dde64a30aa4986752fd10ee0a8a9d57cd523b
davidbuzatu-marian <davidm.buzatu@gmail.com>
2022-02-04 17:51:10 +0000

    Added initial dashboard

--- fyp-frontend/pages/dashboard.js
+++ fyp-frontend/pages/dashboard.js
@@ -1,38 +1,113 @@
 import React, { useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import Link from 'next/link';
-import style from '../styles/Utils.module.scss';
 import { useUser } from '../hooks/user';
 import Router from 'next/router';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const drawerWidth = 240;
+
 const Dashboard = () => {
-  const [user] = useUser();
+  const [user, { loading }] = useUser();
   useEffect(() => {
     console.log(user);
     if (!user) {
-      Router.push('/auth/login?afterLoginRedirect=dashboard');
+      Router.push('/auth/login?afterLoginRedirect=/dashboard');
     }
   }, [user]);
   return (
     <Container
-      maxWidth='md'
+      maxWidth='100%'
       sx={{
         minHeight: '100vh',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        mt: '4rem',
       }}
     >
-      <header>
-        <Typography variant='h3' sx={{ fontWeight: 'bold' }}>
-          Dashboard
-        </Typography>
-      </header>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <header>
+            <Box sx={{ display: 'flex' }}>
+              <AppBar
+                position='fixed'
+                sx={{
+                  width: `calc(100% - ${drawerWidth}px)`,
+                  ml: `${drawerWidth}px`,
+                }}
+              >
+                <Toolbar sx={{ justifyContent: 'end' }}>
+                  <Typography variant='h6' noWrap component='div'>
+                    {user.email}
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <Drawer
+                sx={{
+                  width: drawerWidth,
+                  flexShrink: 0,
+                  '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                  },
+                }}
+                variant='permanent'
+                anchor='left'
+              >
+                <Toolbar>
+                  <Typography variant='h6' noWrap component='div'>
+                    Final Year Project
+                  </Typography>
+                </Toolbar>
+                <Divider />
+                <List>
+                  {[
+                    ['Environments', 'grid_view'],
+                    ['Datasets', 'storage'],
+                  ].map((item, index) => (
+                    <ListItem button key={index}>
+                      <ListItemIcon>
+                        <span className='material-icons'>{item[1]}</span>
+                      </ListItemIcon>
+                      <ListItemText primary={item[0]} />
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider />
+                <List>
+                  {[['Logout', 'logout']].map((item, index) => (
+                    <ListItem button key={index}>
+                      <ListItemIcon>
+                        <span className='material-icons'>{item[1]}</span>
+                      </ListItemIcon>
+                      <ListItemText primary={item[0]} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Drawer>
+            </Box>
+          </header>
+          <section>
+            <Box
+              component='main'
+              sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+              <Typography variant='h3'>Dashboard</Typography>
+            </Box>
+          </section>
+        </>
+      )}
     </Container>
   );
 };
