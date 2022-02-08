c06665c2b555b2978b5e0467892678dadd9fb254
davidbuzatu-marian <davidm.buzatu@gmail.com>
2022-02-08 09:12:00 +0000

    Added training distribution model and getter

--- backend_server/routes/dataset.js
+++ backend_server/routes/dataset.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const EnvironmentDataDistribution = require('../models/EnvironmentDataDistribution');
+const EnvironmentTrainingDataDistribution = require('../models/EnvironmentTrainingDataDistribution');
 const router = express.Router();
 const mongoose = require('mongoose');
 
@@ -9,6 +10,15 @@ router.get('/distribution', async (req, res) => {
     user_id: mongoose.mongo.ObjectId(userId),
   });
   return res.send(environmentsDataDistribution);
+});
+
+router.get('/training/distribution', async (req, res) => {
+  const userId = req.headers['x-auth'];
+  const environmentsTrainingDataDistribution =
+    await EnvironmentTrainingDataDistribution.find({
+      user_id: mongoose.mongo.ObjectId(userId),
+    });
+  return res.send(environmentsTrainingDataDistribution);
 });
 
 module.exports = router;
