d40c74b8b773444af0931353cec9a25ae171470f
unknown <davidm.buzatu@gmail.com>
2022-02-27 11:05:09 +0000

    Added download endpoint for model download

--- backend_server/routes/model.js
+++ backend_server/routes/model.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { strategyMap, QueueStrategy } = require('../workers/strategy');
+const { strategyMap, QueueStrategy } = require("../workers/strategy");
+const path = require("path");
 
 const {
   handleJobResponse,
   createJobBody,
   createJobHeader,
-} = require('../hooks/environment');
-const { removeUrlParams } = require('../hooks/url');
+} = require("../hooks/environment");
+const { removeUrlParams } = require("../hooks/url");
 
-router.post('/*', async (req, res) => {
+router.post("/*", async (req, res) => {
   if (!strategyMap.has(req.originalUrl)) {
-    return res.statusCode(404).send('Endpoint not found');
+    return res.statusCode(404).send("Endpoint not found");
   }
   const routeStrategy = new QueueStrategy();
   routeStrategy.setStrategy(strategyMap.get(req.originalUrl));
-  delete req.headers['content-length'];
-  const jobHeader = createJobHeader(req, 'application/json');
+  delete req.headers["content-length"];
+  const jobHeader = createJobHeader(req, "application/json");
   const jobBody = createJobBody(req);
   const job = await routeStrategy.add({
     headers: jobHeader,
@@ -25,10 +26,27 @@ router.post('/*', async (req, res) => {
   return res.status(202).json({ jobLink: `${req.originalUrl}/${job.id}` });
 });
 
-router.get('/*:id', async (req, res) => {
+router.get("/download", async (req, res) => {
+  if (!req.body || (req.body && !req.body.environment_id)) {
+    return res.status(400).send("Environment id is required");
+  }
+  environment_id = req.body.environment_id;
+  res.download(
+    path.join(
+      __dirname,
+      "..",
+      "workers",
+      "model",
+      "models",
+      `${environment_id}.pth`
+    )
+  );
+});
+
+router.get("/*:id", async (req, res) => {
   const url = removeUrlParams(req.originalUrl);
   if (!strategyMap.has(url)) {
-    return res.status(404).send('Endpoint not found');
+    return res.status(404).send("Endpoint not found");
   }
 
   const routeStrategy = new QueueStrategy();
